c2b7a3886f5121cd0ec938f3dedf3b6f9069959a
Firat Bulduk <firatbulduk74@gmail.com>
2026-09-17 15:10:20 +0200

    EXAM prompter: add chapter 2, interaction Hamiltonian

--- exam/sw.js
+++ exam/sw.js
@@ -1,5 +1,5 @@
 // bump CACHE when index.html changes; the old copy is then dropped
-const CACHE = 'exam-prompter-20260917-125824';
+const CACHE = 'exam-prompter-20260917-131016';
 const ASSETS = ['./', './index.html', './manifest.webmanifest'];
 self.addEventListener('install', e => {
   self.skipWaiting();
